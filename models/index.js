3609f3ae2d6d41c78c1db310614af9a3e22ab0e5
Nick Camacho <118080701+NickCamacho15@users.noreply.github.com>
2023-08-03 12:49:42 -0400

    Added files for application
    Uploaded the necessary files to build application.

---
+++ models/index.js
@@ -0,0 +1,32 @@
+
+const Product = require('./Product');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
+const Category = require('./Category');
+
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
+
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+});
+
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  as: 'tags',
+  foreignKey: 'product_id',
+});
+
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  as: 'products',
+  foreignKey: 'tag_id',
+});
+
+module.exports = {
+  Product,
+  Tag,
+  ProductTag,
+  Category,
+};
